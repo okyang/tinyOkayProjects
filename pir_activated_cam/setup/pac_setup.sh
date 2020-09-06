bbf8dc7d296d013ca6be3aa422be1666012c0709
Owen Yang <owenyang123@gmail.com>
2020-09-05 18:49:21 -0700

    update ignore files

---
+++ pir_activated_cam/setup/pac_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing xterminal"
+sudo apt-get install xterm -y
+echo "Done"
+echo
+
+echo "Installing nanPy"
+pip3 install nanPy -y
+echo "Done"
+echo
+
+echo "Making Dependent files"
+python3 makeDependentFiles.py
+echo "Done"
+echo
+
+
+echo "Setting up .desktop file"
+mkdir /home/pi/.config/autostart
+cp pac_startup.desktop /home/pi/.config/autostart/pac_startup.desktop
+echo "Done"
+echo
