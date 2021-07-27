50b50feba9b859f876ced908b938f0aa853728af
Owen Yang <owenyang123@gmail.com>
2021-07-26 21:38:48 -0700

    update arduino code

--- operation_game/shock_operation/shock_operation.ino
+++ operation_game/shock_operation/shock_operation.ino
@@ -1,26 +1,41 @@
+// initialize variables for used pins
 int relay1 = 2; //D2
 int relay2 = 3; //D3
+
 int tweezers = 12; //D4
-int board = 4; //D12
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(tweezers, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // detect if circuit was closed
   if (digitalRead(tweezers) == LOW) {
-    // Activate the TENS units 
+    Serial.println("Yikes");
+
+    // Activate Relay 1 and Relay 2
     digitalWrite(relay1, HIGH);
     digitalWrite(relay2, HIGH);
+
+    // keep on for 2 seconds
     delay(2000);
+
+    // turn off the relays
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+
+    // keep off for 1 seconds
+    delay(1000);
   }
   else {
+    // turn off the relays
     digitalWrite(relay1, LOW);
     digitalWrite(relay2, LOW);
   }
-  
+
 
 }
